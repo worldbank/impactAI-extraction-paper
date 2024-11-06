5c73a9c39e81b1140a3ce84e8c0fb50f00da9054
agombert <arnault.gombert@pm.me>
2024-11-06 18:18:59 +0100

    :beers: adding evaluation settings class

--- src/rct_clf/settings.py
+++ src/rct_clf/settings.py
@@ -7,8 +7,29 @@ class ZSLSettings:
     path_prompt: Path = Path("config/prompts/RCT_ZSL.prompt")
     path_input: Path = Path("data/processed/metadata.json")
     path_output: Path = Path("data/processed/matadata_rct_classified.json")
-    system_content: str = "You are a an expert in economic research."
+    system_content: str = "You are a an expert in economic research and in classifiying studies as Randomized Controlled Trial (RCT) or not."
     temperature: float = 1.0
     model: str = "gpt-4o"
     max_tokens: int = 1024
     batch_size: int = 10
+
+
+@dataclass
+class TrainingParams:
+    model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
+    max_length: int = 32
+    batch_size: int = 16
+    num_epochs: int = 5
+    learning_rate: float = 2e-5
+    evaluation_strategy: str = "epoch"
+    save_strategy: str = "epoch"
+    load_best_model_at_end: bool = True
+    output_dir: str = "SetFitModel"
+    num_iteration: int = 3
+
+
+@dataclass
+class EvaluationParams:
+    path_preds: Path = Path("data/processed/matadata_rct_classified.json")
+    path_true: Path = Path("data/raw/RCT_GT.csv")
+    path_output: Path = Path("data/processed/ZSL_two_steps_metrics.json")
